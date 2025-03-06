2032a2467deffa60dc4440985d21a845025bcfd7
Denis Gomez <denis.gomez.pineda@gmail.com>
2025-03-05 21:31:50 -0600

    borrar no nce

--- dist/routes/categoria.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routerCategoria = void 0;
-const express_1 = require("express");
-const categoria_controller_1 = require("../controllers/categoria.controller");
-exports.routerCategoria = (0, express_1.Router)();
-exports.routerCategoria.post("/", categoria_controller_1.saveCategoria);
-exports.routerCategoria.get("/", categoria_controller_1.getAllCategorias);
-exports.routerCategoria.delete("/:id", categoria_controller_1.deleteCategoria);
