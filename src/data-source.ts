dbbcc08100dddffea6b4a8c4b9a0d62f3ebe4cc9
Denis Gómez <denis.gomez.pineda@gmail.com>
2025-04-08 06:23:50 -0600

    Agregar dia de la semana

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,14 +8,29 @@ import { Item_carrito } from "./models/Item_carrito"
 import { Orden_compra } from "./models/Orden_compra"
 import { Item_orden_compra } from "./models/Item_orden_compra"
 
+// producción
+const host = "mysql-aerolineaia2025.alwaysdata.net";
+const username = "400640";
+const password = "AlwaysData2025";
+const database = "aerolineaia2025_aerolinea";
+
+
+// pruebas
+/*
+const host = "localhost";
+const username = "root";
+const password = "";
+const database = "ecommerce";
+*/
+
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "mysql-aerolineaia2025.alwaysdata.net",
+    host,
     port: 3306,
-    username: "400640",
-    password: "AlwaysData2025",
-    database: "aerolineaia2025_aerolinea",
+    username,
+    password,
+    database,
     synchronize: true,
     logging: false,
     entities: [Categoria, Producto, Usuario, Carrito, Item_carrito, Orden_compra, Item_orden_compra],
